1bf8352167b16cd90edffd55b5b098f7416afbf0
seongmin kim <k2417000@naver.com>
2024-07-02 16:53:07 +0900

    초기 설정 세팅: gitignore, iOS만 지원, 다크모드만 지원, 코드 베이스 설정

--- RealmPractice/MainViewController.swift
+++ RealmPractice/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  RealmPractice
 //
 //  Created by 김성민 on 7/2/24.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
 }
-
