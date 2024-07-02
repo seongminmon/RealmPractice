b0dd5910710b02603a3aa7e3bd0a554e76147bd6
seongmin kim <k2417000@naver.com>
2024-07-02 17:11:28 +0900

    Main 화면 구성

--- RealmPractice/MainViewController.swift
+++ RealmPractice/MainViewController.swift
@@ -6,11 +6,46 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainViewController: UIViewController {
+    
+    lazy var newTodoButton: UIButton = {
+        var config = UIButton.Configuration.plain()
+        config.title = "새로운 할 일"
+        config.image = UIImage(systemName: "plus.circle.fill")
+        config.imagePadding = 8
+        
+        let button = UIButton(configuration: config)
+        button.addTarget(self, action: #selector(newTodoButtonClicked), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        addSubviews()
+        configureLayout()
+        configureView()
+    }
+    
+    func addSubviews() {
+        view.addSubview(newTodoButton)
+    }
+    
+    func configureLayout() {
+        newTodoButton.snp.makeConstraints { make in
+            make.leading.bottom.equalTo(view.safeAreaLayoutGuide).inset(8)
+            make.width.equalTo(140)
+            make.height.equalTo(30)
+        }
+    }
+    
+    func configureView() {
+        //
+    }
+    
+    @objc func newTodoButtonClicked() {
+        print(#function)
     }
     
 }
